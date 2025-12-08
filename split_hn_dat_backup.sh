6f9cde6e0f5d3e7ab05cf1f42445625d45bc09a0
Celeste Hao <134777729+CelesteHao@users.noreply.github.com>
2025-12-07 21:24:15 -0800

    Add files via upload
    Use bash to split the combined data into daily data.
    Use bash to split the raw data into daily data, 
    Create a data list named ‘dir_list.txt’ contains the data dictionary of raw data; Then change the path and file name in ‘split_hn_dat.sh’ and ‘split_hn_dat_backup.sh’ (in case some ‘.backup’ data caused by interrupted); Then run the ‘.sh’ in WSL using code: 
    cd ‘the path’ (go to the path)
    chmod +x split_hn_dat.sh (mark the .sh as an executable file)
    dos2unix dir_list.txt (modify the .txt file in case of line break) 
    bash split_hn_dat.sh (run the .sh file)
    Then get the daily data.
    (there is a small problem that data of every day begins at 00:00:00 and ends at 23:59:59.9)

---
+++ split_hn_dat_backup.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+shopt -s nocaseglob    # <-- enable case-insensitive filename matching
+
+# BASEDIR="$HOME/Split bash/"
+OUTDIR="/mnt/c/split/hn_1day/"  # Save outputs in WSL home
+
+mkdir -p "$OUTDIR"
+
+# Loop through each directory listed in dir_list.txt
+while read -r DATDIR; do
+    echo "Processing $DATDIR ..."
+
+    # Make sure the directory exists
+    if [ ! -d "$DATDIR" ]; then
+        echo "Directory $DATDIR does not exist, skipping."
+        continue
+    fi
+
+	for FILE in "$DATDIR"/*_TS10Hz.dat.backup; do
+		echo "Processing $FILE"
+
+		head -n 4 "$FILE" > header.tmp
+
+		awk -F, -v outdir="$OUTDIR" '
+		NR > 4 {
+			split($1, dt, " ");
+			date = substr(dt[1], 2);
+			out = outdir "/Hn1_" date "_TS.dat";
+
+			if (!(out in seen)) {
+				if (!system("[ -f " out " ]")) {
+					seen[out] = 1;
+				} else {
+					system("cat header.tmp > " out);
+					seen[out] = 1;
+				}
+			}
+
+			print $0 >> out;
+		}' "$FILE"
+
+		rm -f header.tmp
+	done
+
+	for FILE in "$DATDIR"/*_Met1min.dat.backup; do
+		echo "Processing $FILE"
+
+		head -n 4 "$FILE" > header.tmp
+
+		awk -F, -v outdir="$OUTDIR" '
+		NR > 4 {
+			split($1, dt, " ");
+			date = substr(dt[1], 2);
+			out = outdir "/Hn1_" date "_MET.dat";
+
+			if (!(out in seen)) {
+				if (!system("[ -f " out " ]")) {
+					seen[out] = 1;
+				} else {
+					system("cat header.tmp > " out);
+					seen[out] = 1;
+				}
+			}
+
+			print $0 >> out;
+		}' "$FILE"
+
+		rm -f header.tmp
+	done
+
+	for FILE in "$DATDIR"/*_Soil_1min.dat.backup; do
+		echo "Processing $FILE"
+
+		head -n 4 "$FILE" > header.tmp
+
+		awk -F, -v outdir="$OUTDIR" '
+		NR > 4 {
+			split($1, dt, " ");
+			date = substr(dt[1], 2);
+			out = outdir "/Hn1_" date "_SOIL.dat";
+
+			if (!(out in seen)) {
+				if (!system("[ -f " out " ]")) {
+					seen[out] = 1;
+				} else {
+					system("cat header.tmp > " out);
+					seen[out] = 1;
+				}
+			}
+
+			print $0 >> out;
+		}' "$FILE"
+
+		rm -f header.tmp
+	done
+
+done < dir_list.txt   # <- reads the file line by line
+echo "Done processing all .dat files."
